32538f96030a2dd59fa35de7543561496961e7fe
sardor <sindarovsardor9@gmail.com>
2023-01-09 09:30:57 -0800

    222

--- Js/app.js
+++ Js/app.js
@@ -31,6 +31,32 @@ sigU.addEventListener("click", function () {
 sigI.addEventListener("click", function () {
   window.location = "/index.html";
 });
+//  Time
+setInterval(() => {
+  const vaqt = new Date();
+  let y = vaqt.getFullYear();
+  let o = vaqt.getMonth();
+  let k = vaqt.getDate();
+  let s = vaqt.getHours();
+  let m = vaqt.getMinutes();
+  let se = vaqt.getSeconds();
+  oy = nol(o + 1);
+  document.getElementById("yil").innerHTML = y;
+  document.getElementById("oy").innerHTML = oy;
+  document.getElementById("kun").innerHTML = nol(k);
+  document.getElementById("soat").innerHTML = nol(s);
+  document.getElementById("minut").innerHTML = nol(m);
+  document.getElementById("sekund").innerHTML = nol(se);
+
+   
+}, 1000);
+function nol(i) {
+  if (i < 10) {
+    i = "0" + i;
+  }
+  return i;
+}
+
 // function red() {
 //   let onlin = navigator.onLine;
 //   if(onlin === false){
